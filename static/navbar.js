bcd6aa796080ed35d0e89640a4a9fd9c30be1387
Wenzhou Mei <wenzhoumei7@gmail.com>
2024-04-15 14:50:20 +0100

    made navbar stay a bit at the top when scrolling from the top

--- static/navbar.js
+++ static/navbar.js
@@ -9,24 +9,25 @@ window.onpageshow = () => {
 }
 
 window.onscroll = () => {
-    // Avoid scroll event being automatically triggered by browser
-    if (!initialScroll) {
-        var currScrollY = window.scrollY;
-
-        // When usr scrolls up, don't hide navbar
-        if (prevScrollY > currScrollY) {
-            topbar.style.top = "0";
-            topbar.style.boxShadow = "0px 1px 2px rgba(0, 0, 0, 0.9)";
-        // Otherwise hide navbar
-        } else {
-            topbar.style.top = "-80px";
-            topbar.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.0)";
-        }
-    
-        prevScrollY = currScrollY;
-    }
+    var currScrollY = window.scrollY;
+    if (currScrollY > 20) {
+      // Avoid scroll event being automatically triggered by browser
+      if (!initialScroll) {
+          // When usr scrolls up, don't hide navbar
+          if (prevScrollY > currScrollY) {
+              topbar.style.top = "0";
+              topbar.style.boxShadow = "0px 1px 2px rgba(0, 0, 0, 0.9)";
+          // Otherwise hide navbar
+          } else {
+              topbar.style.top = "-80px";
+              topbar.style.boxShadow = "0px 0px 0px rgba(0, 0, 0, 0.0)";
+          }
+      
+          prevScrollY = currScrollY;
+      }
     
     initialScroll = false;
+  }
 }
 
 navlinks = document.getElementsByClassName("navlink");
